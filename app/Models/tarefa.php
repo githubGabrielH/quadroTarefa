c480f886d202470efee0f02d5de6e8b932d30a0d
Gabriel Henrique De Souza Santos <23432@etehort.ps.sp>
2024-11-21 22:16:26 -0300

    termino da tarefa

--- app/Models/tarefa.php
+++ app/Models/tarefa.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tarefa extends Model
+
+class Tarefa extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+    // Permite o preenchimento em massa para esses campos
+    
     protected $fillable = [
         'titulo',
         'descricao'
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TarefaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tarefas', [TarefaController:: class, 'index'])->name('tarefas.index');
+Route::get('/tarefas/create', [TarefaController:: class, 'create'])->name('tarefas.create');
+Route::post('/tarefas', [TarefaController:: class, 'store'])->name('tarefas.store');
+Route::get('/tarefas{id}/edit', [TarefaController:: class, 'edit'])->name('tarefas.edit');
+Route::get('/tarefas{id}/show', [TarefaController:: class, 'show'])->name('tarefas.show');
+Route::put('/tarefas/{id}', [TarefaController:: class, 'update'])->name('tarefas.update');
+Route::delete('/tarefas/{id}', [TarefaController:: class, 'destroy'])->name('tarefas.destroy');
